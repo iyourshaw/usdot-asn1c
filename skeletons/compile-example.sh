90fd0e3bdeefa48504dca4c8eb6b7fd94295b13f
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2026-07-18 18:33:59 -0600

    -Wno-format flag

--- skeletons/compile-example.sh
+++ skeletons/compile-example.sh
@@ -3,7 +3,7 @@
 SHARE=/usr/local/share/asn1c/include
 
 # Compile generated files and converter example
-gcc -I$SHARE -DASN_PDU_COLLECTION -DHAVE_TM_GMTOFF -c *.c
+gcc -I$SHARE -DASN_PDU_COLLECTION -DHAVE_TM_GMTOFF -Wno-format -c *.c
 
 # Link compiled object files with pre-compiled skeleton library libskeleton.a to make executable
 gcc *.o -I$SHARE -I. -L$SHARE -lskeleton -o converter-example
